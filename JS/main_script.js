5bf80ad572998722b5ff4f0898d5be1f6312b45c
Matt-Breithaupt <Matt-Breithaupt@users.noreply.github.com>
2017-11-07 11:17:25 -0600

    Inserted HighCharts.chart 'area-negative-demo'

--- JS/main_script.js
+++ JS/main_script.js
@@ -7,6 +7,35 @@ AOS.init({
 
 
 
+Highcharts.chart('area-negative-demo', {
+    chart: {
+        type: 'area'
+    },
+    title: {
+        text: 'Area chart with negative values'
+    },
+    xAxis: {
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'John',
+        data: [5, 3, 4, 7, 2]
+    }, {
+        name: 'Jane',
+        data: [2, -2, -3, 2, 1]
+    }, {
+        name: 'Joe',
+        data: [3, 4, 4, -2, 5]
+    }]
+});
+
+
+
+
+
 /*NAVBAR*/
 !(function(o) {
   "use strict";
