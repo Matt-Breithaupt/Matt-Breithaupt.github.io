95e1e3f9df79e09896eac443ea4c167481fc8b38
Matt-Breithaupt <Matt-Breithaupt@users.noreply.github.com>
2017-12-01 14:29:34 -0600

    Profile overlay on click script

--- JS/main_script.js
+++ JS/main_script.js
@@ -1,3 +1,15 @@
+/*PROFILE: overlay show/hide on click: jQuery*/
+ $(function() {
+   var $divs = $('.divs > div');
+   $divs.first().show()
+   $('input[type=radio]').on('change', function() {
+     $divs.hide();
+     $divs.eq($('input[type=radio]').index(this)).show();
+   });
+ });
+
+
+
 /*Animate On Scroll (library)*/
 AOS.init({
   disable: "mobile",
