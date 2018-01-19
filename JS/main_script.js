062e5465d24e55f19268209bb91f5fa1fadf58a2
Matt-Breithaupt <Matt-Breithaupt@users.noreply.github.com>
2018-01-18 18:12:16 -0600

    Update main_script.js

--- JS/main_script.js
+++ JS/main_script.js
@@ -7,6 +7,14 @@
 /*<script>(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")</script>*/
 
 
+/*PAGE PRELOADING ICON (replacement for splash screen)*/
+<script>
+$(window).load(function(){
+     $('.loader').fadeOut("slow", "swing");
+});
+
+
+
 /*PROFILE: overlay show/hide on click: jQuery*/
  $(document).ready(function() {
    var $divs = $('.divs > div');
