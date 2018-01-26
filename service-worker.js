4b3b963fb102ddb7d4da8918d287bf5797910a5f
Matt-Breithaupt <Matt-Breithaupt@users.noreply.github.com>
2018-01-26 10:14:19 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,6 @@
-
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  'https://matt-breithaupt.github.io/index.html',
+  'https://matt-breithaupt.github.io/CSS/main.css',
+  'https://matt-breithaupt.github.io/JS/main_script.js'
+];
