217d1250609d68899a2d23c84e48e05a3cf6de78
Matt-Breithaupt <Matt-Breithaupt@users.noreply.github.com>
2018-01-26 13:19:28 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   'https://matt-breithaupt.github.io/index.html',
-  'https://rawgit.com/Matt-Breithaupt/Matt-Breithaupt.github.io/master/CSS/main.css',
-  'https://rawgit.com/Matt-Breithaupt/Matt-Breithaupt.github.io/master/JS/main_script.js'
+  'https://matt-breithaupt.github.io/CSS/main.css',
+  'https://matt-breithaupt.github.io/JS/main_script.js'
 ];
 
 self.addEventListener('install', function(event) {
