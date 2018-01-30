a49ec4eb5e29b2cce4cc0e8b9e9e24c667ea165e
Matt-Breithaupt <Matt-Breithaupt@users.noreply.github.com>
2018-01-30 11:25:23 -0600

    Update main_script.js

--- JS/main_script.js
+++ JS/main_script.js
@@ -70,8 +70,9 @@ AOS.init({
 
 
 /*USER JOURNEY CHART V2: render to*/
-  $(function () {
-    var chart = new Highcharts.Chart({
+  var chart;
+    $(document).ready(function() {
+      chart = new Highcharts.Chart({
         chart: {
           renderTo: "container6",
           defaultSeriesType: "areaspline"
