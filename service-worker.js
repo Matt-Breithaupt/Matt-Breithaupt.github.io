3fa2635368fdfd590910d1fb9113535d24c8acd9
MattBreithaupt <24234154+MattBreithaupt@users.noreply.github.com>
2018-03-22 14:35:23 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,7 @@ var urlsToCache = [
   'https://mattbreithaupt.com/CSS/main-dist.min.css',
   'https://mattbreithaupt.com/CSS/dep/dependencies-dist.min.css',
   'https://mattbreithaupt.com/JS/concat_directions-dist.min.js',
-  'https://mattbreithaupt.com/favicon.ico,'
-  'https://mattbreithaupt.com/manifest.json'
+  'https://mattbreithaupt.com/favicon.ico'
 ];
 
 self.addEventListener('install', function(event) {
