66d47e3ffad10685a8c7a9fa6ea996f81bc0cdfc
MattBreithaupt <24234154+MattBreithaupt@users.noreply.github.com>
2018-03-23 08:39:56 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -41,6 +41,7 @@ self.addEventListener('fetch', function(event) {
             var responseToCache = response.clone();
 
             caches.open(CACHE_NAME)
+              debugger;
               .then(function(cache) {
                 cache.put(event.request, responseToCache);
               });
