58d8b06528e86692883e7cfe767f82c77aa605c3
MattBreithaupt <24234154+MattBreithaupt@users.noreply.github.com>
2018-03-23 09:19:53 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -42,26 +42,10 @@ self.addEventListener('fetch', function(event) {
 
             caches.open(CACHE_NAME)
               .then(function(cache) {
-                cache.put(event.request, responseToCache);
+                cache.put(event.request);
               });
 
             return response;
-            
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
             
           }
         );
